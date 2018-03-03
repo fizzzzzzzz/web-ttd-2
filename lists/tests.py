3d2618c68e34707999fd303967829c8b5ee3603a
Yu <294109348@qq.com>
2018-03-03 21:00:57 +0800

    shi bu guo san

--- lists/tests.py
+++ lists/tests.py
@@ -5,9 +5,16 @@ from django.template.loader import render_to_string
 
 from lists.views import home_page
 
+import re
+
 # Create your tests here.
 class HomePageTest(TestCase):
 	
+	@staticmethod
+	def remove_csrf(html_code):
+		csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+		return re.sub(csrf_regex, '', html_code)
+		
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
@@ -16,4 +23,14 @@ class HomePageTest(TestCase):
 		request = HttpRequest()
 		response = home_page(request)
 		expected_html = render_to_string('home.html')
-		self.assertEqual(response.content.decode(), expected_html)
+		self.assertEqual(self.remove_csrf(response.content.decode()), self.remove_csrf(expected_html))
+		
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+		
+		response = home_page(request)
+		self.assertIn('A new list item', response.content.decode())
+		expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
+		self.assertEqual(self.remove_csrf(response.content.decode()), self.remove_csrf(expected_html))
